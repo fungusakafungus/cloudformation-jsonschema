90eee92498204eee5b1e8aa514c4c2fbfa5b9903
Ilya Margolin <ilya@ulani.de>
2014-04-27 23:15:08 +0200

    Style

--- resource_properties.py
+++ resource_properties.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 from pyquery import PyQuery as q
-import jsonschema
-import json
 from collections import OrderedDict
 from tools import *
-from logging import *
+from logging import warn
+
+
+PROPERTIES_REFERENCE = 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-product-property-reference.html'
 
 
 def property_name_from_href(href):
@@ -12,8 +13,13 @@ def property_name_from_href(href):
     href, _ = href.rsplit(".", 1)
     _, href = href.rsplit("/", 1)
     return href
+
+
 def property_ref_from_href(href):
-    return {'$ref': '#/definitions/property_types/%s' % property_name_from_href(href)}
+    return {
+        '$ref':
+        '#/definitions/property_types/%s' % property_name_from_href(href)
+    }
 
 
 def get_type(dd_):
@@ -32,31 +38,40 @@ def get_type(dd_):
     if dd_('.type') and len(dd_('.type')):
         if (dd_('.type').text() == 'AWS::EC2::SecurityGroup' and
                 'list of' in t):
-            return {'type':'array', 'items': {'type':'string'}}
+            return {'type': 'array', 'items': {'type': 'string'}}
 
     warn('Could not parse resource property type: "%s"', dd_.html())
     return {}
 
 all_properties = all_resource_properties_hrefs()
 
+
 def set_resource_properties(res_type):
     all = all_resource_hrefs()
-    h=get_pq(all[res_type])
-    schema=load()
+    h = get_pq(all[res_type])
+    schema = load()
     dl = h('#divContent .variablelist dl')
-    resources=resources_dict(schema)
-    pairs=zip(dl('dt'),dl('dd'))
-    pairs=[(q(dt),q(dd)) for dt,dd in pairs]
+    resources = resources_dict(schema)
+    pairs = zip(dl('dt'), dl('dd'))
+    pairs = [(q(dt), q(dd)) for dt, dd in pairs]
     shortcut = resources[res_type]['properties']
     shortcut['Properties'] = OrderedDict()
-    shortcut['Properties']['properties'] = OrderedDict((dt.text(),get_type(dd)) for dt,dd in pairs)
-    required=[k.text() for k,v in pairs if v('p').filter(lambda i: 'Required : Yes' in q(this).text())]
-    shortcut['Properties']['required']=required
+    shortcut['Properties']['properties'] = OrderedDict(
+        (dt.text(), get_type(dd))
+        for dt, dd in pairs
+    )
+    required = [
+        k.text()
+        for k, v
+        in pairs
+        if v('p').filter(lambda i: 'Required : Yes' in q(this).text())
+    ]
+    shortcut['Properties']['required'] = required
     return schema
 
 
 def all_properties():
-    h = get_pq('https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-product-property-reference.html')
+    h = get_pq(PROPERTIES_REFERENCE)
     res = OrderedDict(
         (
             property_name_from_href(q(a).attr("href")),
