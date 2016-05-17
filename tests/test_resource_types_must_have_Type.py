466ad76cffcd21efd6af782a680d5f0f7d71da26
Ilya Margolin <ilya@ulani.de>
2016-05-18 01:21:27 +0200

    Fix tests

--- tests/test_resource_types_must_have_Type.py
+++ tests/test_resource_types_must_have_Type.py
@@ -1,29 +1,27 @@
-valid = """
-{
-    "Type" : "AWS::IAM::User",
-    "Properties" : {
-        "Path" : "/"
-    }
-}
-"""
-
-invalid = """
-{
-    "Properties" : {
-        "Path" : "/"
-    }
-}
-"""
-
 import jsonschema
 import json
 import pytest
 
+valid = {
+    "Type": "AWS::IAM::User",
+    "Properties": {
+        "Path": "/"
+    }
+}
+
+invalid = {
+    "Properties": {
+        "Path": "/"
+    }
+}
+
 resource_schema = json.load(open('resource.json'))
+
+
 def test_valid():
-    jsonschema.validate(json.loads(valid), resource_schema)
+    jsonschema.validate(valid, resource_schema)
+
 
 def test_invalid():
-    with pytest.raises(jsonschema.ValidationError) as excinfo:
-        jsonschema.validate(json.load(open('tests/no-type.json')), resource_schema)
-    assert "'Type' is a required property" in excinfo.value.message
+    with pytest.raises(jsonschema.ValidationError):
+        jsonschema.validate(invalid, resource_schema)
