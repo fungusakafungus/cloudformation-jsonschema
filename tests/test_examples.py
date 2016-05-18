18f99766210c8a96636fccd82c81a5863402f618
Ilya Margolin <ilya@ulani.de>
2016-05-18 12:23:46 +0200

    Useful error messages!

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -13,7 +13,23 @@ schema_str = schema_str.replace(
 schema = json.loads(schema_str)
 
 
+def validate_resource(resource, schema):
+    resource_types = schema['definitions']['resource_types']
+    resource_type = resource['Type']
+    if resource_type not in resource_types.keys():
+        raise ValueError('Resource type "{}" unknown'.format(resource_type))
+    resource_schema = resource_types[resource_type].copy()
+    resource_schema['definitions'] = schema['definitions']
+    jsonschema.validate(resource, resource_schema)
+
+
 @pytest.mark.parametrize("template", glob.glob('tests/examples/*.template'))
 def test_fn_base64_valid(template):
     instance = json.load(open(template))
-    jsonschema.validate(instance, schema)
+    try:
+        jsonschema.validate(instance, schema)
+    except jsonschema.ValidationError as e:
+        if '/'.join(e.absolute_schema_path) == 'properties/Resources/patternProperties/^[a-zA-Z0-9]+$/oneOf':
+            validate_resource(e.instance, e.schema)
+        else:
+            raise
