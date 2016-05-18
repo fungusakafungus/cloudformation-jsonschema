9bbc8f118904a76991d05bd208efc0ddf9129f9c
Ilya Margolin <ilya@ulani.de>
2016-05-19 01:43:35 +0200

    Actually use the moved examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,7 +7,7 @@ import val
 schema = json.load(open('schema.json'))
 
 
-@pytest.mark.parametrize("template", glob.glob('tests/examples/*.template'))
+@pytest.mark.parametrize("template", glob.glob('tests/examples-*/*.template'))
 def test_template(template):
     if 'OpenStack' in template:
         pytest.skip('OpenStack is not supported')
