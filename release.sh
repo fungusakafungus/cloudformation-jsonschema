7717e0af6110ab467be6db3c3be2147b9cc36662
Ilya Margolin <ilya@ulani.de>
2016-05-19 15:20:19 +0200

    Add release script

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+test -z "$1" && { echo Usage: $0 v0.1 >&2; exit 2; }
+
+VERSION=$1
+SCHEMA_FILES="schema.json resource.json basic_types.json"
+
+git checkout "$VERSION" -- $SCHEMA_FILES
+for f in $SCHEMA_FILES; do
+	sed -i -f- $f <<-'SEDSCRIPT'
+		1i\
+		---\
+		---
+		3i\
+		    "id": "{{site.github.url}}{{page.url}}",
+	SEDSCRIPT
+done
+mkdir -p $VERSION
+mv $SCHEMA_FILES $VERSION
+git add $VERSION
